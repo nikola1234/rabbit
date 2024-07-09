90b4226ede4072c9497073eaa36fbbef76b68881
punch <398371384@qq.com>
2024-07-09 16:26:24 +0800

    remove test code

--- src/algorithmSwU/src/SequentialList.cc
+++ src/algorithmSwU/src/SequentialList.cc
@@ -1,7 +1,6 @@
 #include "SequentialList.h"
 #include "Logging.h"
 #include <iostream>
-#include <vector>
 
 void SequentialList::testrun(void)
 {
@@ -12,14 +11,9 @@ bool runSequentialListTest()
 {
     SequentialList s;
     s.testrun();
-
-    std::tuple<int, double, std::string> myTuple = {1, 2.2, "three"};
-
-    LOG_INFO << myTuple;
-
     return true;
 }
 
 namespace nm_SequentialListTest{
-    bool _ = runSequentialListTest();
+    // bool _ = runSequentialListTest();
 }
